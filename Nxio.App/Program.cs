5bc99794756a04aa88deb27533381dcd502b70ee
Hubert@Home <h.zielinski.business@gmail.com>
2024-11-30 19:20:38 +0100

    feat: Add roulette module

--- Nxio.App/Program.cs
+++ Nxio.App/Program.cs
@@ -1,5 +1,9 @@
-﻿using NetCord.Gateway;
+﻿using NetCord;
+using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Services.ApplicationCommands;
 
 namespace Nxio.App;
 
@@ -8,12 +12,19 @@ public static class Program
     public static async Task Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
-        builder.Services.AddDiscordGateway(op =>
-        {
-            op.Intents = GatewayIntents.All; // TODO: Use explicit intents instead
-        }).AddGatewayEventHandlers(typeof(Program).Assembly);
 
-        var host = builder.Build().UseGatewayEventHandlers();
+        builder.Services
+            .AddGatewayEventHandlers(typeof(Program).Assembly)
+            .AddApplicationCommands<ApplicationCommandInteraction, ApplicationCommandContext>()
+            .AddDiscordGateway(op =>
+            {
+                op.Intents = GatewayIntents.All; // TODO: Use explicit intents instead
+            });
+
+        var host = builder
+            .Build()
+            .AddModules(typeof(Program).Assembly)
+            .UseGatewayEventHandlers();
 
         await host.RunAsync();
     }
