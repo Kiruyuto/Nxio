13546fb22bbbd8c3cc16da2e79ff66afd86c8ba9
Hubert@Home <h.zielinski.business@gmail.com>
2024-11-30 04:41:17 +0100

    feat: Init project

---
+++ Nxio.App/Handlers/MessageReactionAdd.cs
@@ -0,0 +1,15 @@
+﻿using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+using NetCord.Rest;
+
+namespace Nxio.App.Handlers;
+
+[GatewayEvent(nameof(GatewayClient.MessageReactionAdd))]
+public class MessageReactionAdd(RestClient client, ILogger<MessageReactionAdd> logger) : IGatewayEventHandler<MessageReactionAddEventArgs>
+{
+    public ValueTask HandleAsync(MessageReactionAddEventArgs args)
+    {
+        logger.LogInformation("{Reaction}", $"<@{args.UserId}> reacted {args.Emoji.Name}!");
+        return ValueTask.CompletedTask;
+    }
+}
