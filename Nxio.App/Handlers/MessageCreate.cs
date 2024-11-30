af61a3ccb2e6e378d72d02cffc8552deb6c649dc
Hubert@Home <h.zielinski.business@gmail.com>
2024-11-30 04:41:17 +0100

    feat(bot): Init project

---
+++ Nxio.App/Handlers/MessageCreate.cs
@@ -0,0 +1,14 @@
+﻿using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+
+namespace Nxio.App.Handlers;
+
+[GatewayEvent(nameof(GatewayClient.MessageCreate))]
+public class MessageCreate(ILogger<MessageCreate> logger) : IGatewayEventHandler<Message>
+{
+    public ValueTask HandleAsync(Message msg)
+    {
+        logger.LogInformation("{Content}", msg.Content);
+        return ValueTask.CompletedTask;
+    }
+}
---
+++ Nxio.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+
+namespace Nxio.App;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddDiscordGateway(op =>
+        {
+            op.Intents = GatewayIntents.All; // TODO: Use explicit intents instead
+        }).AddGatewayEventHandlers(typeof(Program).Assembly);
+
+        var host = builder.Build().UseGatewayEventHandlers();
+
+        await host.RunAsync();
+    }
+}
